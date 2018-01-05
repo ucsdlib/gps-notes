00cc0ba251aaa3ffb3705872106a96a19a6ef39f
reid otsuji <rotsuji@ucsd.edu>
2018-01-05 00:15:17 -0800

    syncing master

--- data_mgmt/create_flightsdb.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-library(nycflights13)
-#install.packages('RSQLite')
-library(RSQLite)
-nycfights_gps <- src_sqlite("data_mgmt/nycfights_gps.sqlite3", create = T) 
-flights_sqlite <- copy_to(nycfights_gps, flights, temporary = FALSE, 
-                          indexes = list(c("year", "month", "day"), "carrier", "tailnum"))
