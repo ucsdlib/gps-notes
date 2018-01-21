6d716c7f91bb3be485d24e510557823709a4cb1d
reid otsuji <rotsuji@ucsd.edu>
2018-01-21 01:51:22 -0800

    adding notes for week3
    func, dplyr-tidyr

---
+++ 2018/intro-r/functions/functions.R
@@ -0,0 +1,34 @@
+
+# Takes a dataset and multiplies the population column
+# with the GDP per capita column.
+calcGDP <- function(dat, year=NULL, country=NULL) {
+  if(!is.null(year)) {
+    dat <- dat[dat$year %in% year, ]
+  }
+  if (!is.null(country)) {
+    dat <- dat[dat$country %in% country,]
+  }
+  gdp <- dat$pop * dat$gdpPercap
+  
+  new <- cbind(dat, gdp=gdp)
+  return(new)
+}
+
+
+# Takes a dataset and multiplies the population column
+# with the GDP per capita column.
+calcGDP <- function(dat){
+  gdp <- dat$pop * dat$gdpPercap
+  return(gdp)
+}
+
+# conversions
+fahr_to_kelvin <- function(temp) {
+  kelvin <- ((temp - 32) * (5 / 9)) + 273.15
+  return(kelvin)
+}
+
+kelvin_to_celsius <- function(temp) {
+  celsius <- temp - 273.15
+  return(celsius)
+}
